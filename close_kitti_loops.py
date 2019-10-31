752f5e39ac71a41ce7bae04154d5dc1974e96288
nmerrill67 <nmerrill@udel.edu>
2019-10-31 01:07:38 -0400

    Add nice display for close_kitti_loops

--- close_kitti_loops.py
+++ close_kitti_loops.py
@@ -15,6 +15,7 @@ K = 7
 N = 200
 C = 7
 W = 9
+
 def close_loop(db, dbkp, descr, kp):
     matcher = cv2.BFMatcher(cv2.NORM_L2)
     kp, kp_d = kp
@@ -26,6 +27,7 @@ def close_loop(db, dbkp, descr, kp):
     max_sim = -1.0
     i_max_sim = -1
     best_match_tuple = None
+
     for k in top_k_sim_ind:
         db_kp, db_kp_d = dbkp[k]
         matches = matcher.knnMatch(kp_d, db_kp_d, 2)
@@ -54,7 +56,7 @@ def close_loop(db, dbkp, descr, kp):
             i_max_sim = -1
     return i_max_sim
     
-def kitti_loop(vo_fn, im_dir, seq):	
+def kitti_loop(gui, vo_fn, im_dir, seq):	
     i = 0
     loops = []
     db = []
@@ -64,6 +66,7 @@ def kitti_loop(vo_fn, im_dir, seq):
     last_loop_id = -1
     skipped = False
     j = 0
+
     with open(vo_fn, 'r') as vo_f, open('kitti_traj.txt', 'w') as t_f, \
             open('kitti_loops.txt', 'w') as l_f, tf.Session() as sess:
         
@@ -80,18 +83,20 @@ def kitti_loop(vo_fn, im_dir, seq):
                 y = line_split[7]
                 pts.append([float(x), float(y)])
                 t_f.write(frame_id + ',' + x + ',' + y + '\n')
-                    
+     
                 fl_nm = str(i).zfill(6) + ".png"
                 im = cv2.imread(join(im_dir, "image_2/"+ fl_nm))
+                im_cp = np.copy(im)
+                ims.append(im_cp)
+                im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
                 if im is None or im.shape[0] == 0:
                     print("No image: %s" % fl_nm)
                     break
                 
-                ims.append(im)
                 w = int(4.0 / 3 * im.shape[0]) // 2
                 _w = im.shape[1] // 2
                 #im = im[:, (_w-w):(_w+w+1),:]
-                im = cv2.cvtColor(cv2.resize(im, (vw, vh)), cv2.COLOR_BGR2RGB)
+                im = cv2.resize(im, (vw, vh))
                 t0 = time()
                 
                 descr, c5 = calc.run(im)
@@ -125,7 +130,10 @@ def kitti_loop(vo_fn, im_dir, seq):
                     avg_rate += (rate - avg_rate) / (i+1)
                     print("Frame %d, rate = %f Hz, avg rate = %f Hz" % (i, rate, avg_rate))
                 
+                is_loop = False
+                loop_im = None
                 if loop_count >= C:
+                    is_loop = True
                     print("LOOP DETECTED: %d -> %d" % (i,j))
                     ii = len(pts)-C//2-1
                     jj = j-W//2
@@ -136,17 +144,74 @@ def kitti_loop(vo_fn, im_dir, seq):
                     skipped = False
                     match = np.concatenate((ims[ii], ims[jj]), axis=1)
                     cv2.imwrite('plots/match_kitti%s_%d_%d.png' % (seq, ii, jj), match)
-                    # remove loopp descr since we have revisited this location
-                    db = db[:jj] + db[jj+W//2:]
-                    pts = pts[:jj] + pts[jj+W//2:]
-                    ims = ims[:jj] + ims[jj+W//2:]
+                    # remove loop descr since we have revisited this location
+                    #db = db[:jj] + db[jj+W//2:]
+                    #pts = pts[:jj] + pts[jj+W//2:]
+                    loop_im = ims[jj]
+                    # ims = ims[:jj] + ims[jj+W//2:]
+
+                gui.update(float(x), float(y), is_loop, im_cp, loop_im)
+                
+
     with open('kitti_q_times.txt', 'w') as q_f:
         q_f.writelines(qt)
 
-if __name__ == '__main__':
-    root = "/mnt/f3be6b3c-80bb-492a-98bf-4d0d674a51d6/kitti_odom/"
-    seq = "6"
-    vo_fn = root+"dataset/poses/" + seq.zfill(2) + ".txt"
-    im_dir = root+"dataset/sequences/" + seq.zfill(2) 
+class Display:
+    def __init__(self):
+        self.w = 2*640
+        self.h = 2*480
+        self.canvas = np.zeros((self.h, self.w, 3), dtype=np.uint8)
+        self.x_bounds = [-50.0, 10.0]
+        self.y_bounds = [-7.0, 7.0]
+        self.xs = [0.]
+        self.ys = [0.]
+        self.match = False
 
-    kitti_loop(vo_fn, im_dir, seq)
+    def world2canvas(self, x, y):
+        x_bounds = self.x_bounds
+        y_bounds = self.y_bounds
+        new_x = int(self.w*(x - x_bounds[0])/(x_bounds[1] - x_bounds[0]))
+        new_y = int(self.h*(y - y_bounds[0])/(y_bounds[1] - y_bounds[0]))
+        return new_x, new_y
+
+    def update(self, x, y, is_loop, _im, _loop_im = None):
+        x0, y0 = self.world2canvas(self.xs[-1], self.ys[-1])
+        x1, y1 = self.world2canvas(x, y)
+        cv2.line(self.canvas, (x0,y0), (x1,y1), color=(255,0,0), thickness=7)
+        self.xs.append(x)
+        self.ys.append(y)
+
+        sc = .4
+        h,w = (int(sc*_im.shape[0]),int(sc*_im.shape[1]))
+        im = cv2.resize(_im, (w,h))
+        self.canvas[100:(100+h), :w, :] = im
+        
+        cv2.putText(self.canvas, "Query", (0,50), cv2.FONT_HERSHEY_COMPLEX, 1.0, (255,255,255))
+        cv2.putText(self.canvas, "Match", (0,150+h), cv2.FONT_HERSHEY_COMPLEX, 1.0, (255,255,255))
+
+        if is_loop:
+            r = 7
+            cv2.circle(self.canvas, (x1,y1), r, color=(0,0,255), thickness=-1)
+            loop_im = cv2.resize(_loop_im, (w,h))
+            self.match = True
+            self.canvas[(200+h):(200+2*h), :w, :] = loop_im
+        elif self.match == False:
+            loop_im = 30*np.ones((h,w,3),dtype=np.uint8)
+            cv2.putText(loop_im, "No Match", (66,h//2), cv2.FONT_HERSHEY_COMPLEX, 2.0, (255,255,255))
+            self.canvas[(200+h):(200+2*h), :w, :] = loop_im
+
+        cv2.imshow("Loops", self.canvas)
+        cv2.waitKey(1)
+
+if __name__ == '__main__':
+
+    data_root = "/mnt/f3be6b3c-80bb-492a-98bf-4d0d674a51d6/kitti_odom/"
+    seq = "6"
+    vo_fn = data_root + "dataset/poses/" + seq.zfill(2) + ".txt"
+    im_dir = data_root + "dataset/sequences/" + seq.zfill(2) 
+
+    gui = Display()
+
+    kitti_loop(gui, vo_fn, im_dir, seq)
+
+
